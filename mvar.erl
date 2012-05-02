27d31b993760b4f8dfaed34eb8eaca0410581e67
Mike Blume <mike@loggly.com>
2012-05-02 00:21:17 -0700

    add an mvar and a queue

---
+++ mvar.erl
@@ -0,0 +1,44 @@
+-module(mvar)
+
+listen_full(Data) ->
+    receive
+        {Pid, take} ->
+            Pid !! {self(), Data}
+            listen_empty()
+        {Pid, check} ->
+            Pid !! {self(), true}
+            listen_full(Data)
+    end.
+
+listen_empty() ->
+    receive
+        {Pid, {set, Data}} ->
+            Pid !! {self(), ok}
+            listen_full(Data)
+        {Pid, check} ->
+            Pid !! {self(), false}
+            listen_empty()
+    end.
+
+empty_mvar() ->
+    spawn(?Module, listen_empty, [])
+
+set_mvar(MVar, Data) ->
+    MVar !! {self(), {set, Data}}
+    receive
+        {MVar, ok} -> ok
+    end.
+
+take_mvar(MVar) ->
+    MVar !! {self(), take}
+    receive
+        {MVar, Data} -> Data
+    end.
+
+check_mvar(MVar) ->
+    MVar !! {self(), check} ->
+        receive
+            {MVar, State} -> State
+        end.
+
+
---
+++ queue.erl
@@ -0,0 +1,42 @@
+-module(queue)
+
+listen_empty() ->
+    receive
+        {data, Data} ->
+            listen_full(Data)
+        {command, Pid, empty} ->
+            Pid !! {self(), true}
+            listen_empty()
+    end.
+
+listen_full(Data) ->
+    receive
+        {command, Pid, empty} ->
+            Pid !! {self(), true}
+            listen_full(Data)
+        {command, Pid, pop} ->
+            Pid !! {self(), Data}
+            listen_empty()
+    end.
+
+new_queue() ->
+    spawn(?Module, listen_empty, [])
+
+put_queue(Queue, Data) ->
+    Queue !! {data, Data}
+
+pop_queue(Queue) ->
+    Queue !! {command, self(), pop}
+    receive
+        {Queue, Data} ->
+            Data
+    end.
+
+empty_queue(Queue) ->
+    Queue !! {command, self(), empty}
+    receive
+        {Queue, State} ->
+            State
+    end.
+
+
